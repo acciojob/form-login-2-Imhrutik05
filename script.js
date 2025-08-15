c4975aef03c64eb166c36708badd173a47b4ad77
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-15 23:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById('userForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	const firstName = document.getElementById('fname').value;
+	const lastName = document.getElementById('lname').value;
+	const phoneNumber = document.getElementById('phone').value;
+	const emailID = document.getElementById('email').value;
+
+	alert(
+		`First Name: ${firstName} ` +
+		`Last Name: ${lastName} ` + 
+		`Phone Number: ${phoneNumber} ` +
+		`Email ID: ${emailID}`
+	);
+});
